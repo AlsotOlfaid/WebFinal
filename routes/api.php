6f6764bb36e43cbc8dd99652d76d9fa8080dc648
Emilio <al03044367>
2025-05-06 10:06:29 -0600

    tr15

--- routes/api.php
+++ routes/api.php
@@ -21,8 +21,9 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/words/{categoryId}/{wordsCount}',[WordController::class, 'getWords']);
 
     //Categorias
-    Route::get('/categories', [CategoryController::class, 'index']);
     Route::get('/categories/{id}', [CategoryController::class, 'show']);
+    Route::get('/categories', [CategoryController::class, 'index']);
+    
 
     //Logout
     Route::post('/logout', [AuthController::class, 'logout']);
