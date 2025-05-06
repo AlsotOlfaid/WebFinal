a04b0611c997b70d3e3c780b30b35f1bc932d4c7
Emilio <al03044367>
2025-05-06 10:47:02 -0600

    tr16

---
+++ database/seeders/word_seeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class word_seeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $path = storage_path('app/CategoriesFinalProj.csv');
+
+        $file = fopen($path, 'r');
+
+        // Lee la cabecera
+        $headers = fgetcsv($file);
+
+        while (($row = fgetcsv($file)) !== false) {
+            $data = array_combine($headers, $row);
+
+            DB::table('categories')->insert([
+                'name' => $data['name'],
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
+
+        fclose($file);
+    }
+}
